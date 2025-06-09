a2dc273b481de8c28b7d26414b4323de7d4ed133
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2025-06-09 16:15:37 -0300

    Update cadastrar_categorias.php

--- administrador/cadastrar_categorias.php
+++ administrador/cadastrar_categorias.php
@@ -44,6 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <title>Cadastro de Categoria</title>
      <link rel="stylesheet" href="../css/menu.css">
+    <link rel="stylesheet" href="../css/cadastrar_administrador.css">
   
 </head>
 <body>
